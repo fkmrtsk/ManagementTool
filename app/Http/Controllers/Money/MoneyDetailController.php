d25616630e452510f34e10c49f768e7d37a97a0a
fkmrtsk <ivangattuso0109@gmail.com>
2020-02-24 15:34:47 +0900

    貯金額登録ページ作成

--- app/Http/Controllers/Money/MoneyDetailController.php
+++ app/Http/Controllers/Money/MoneyDetailController.php
@@ -6,14 +6,16 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models;
 use Carbon\Carbon;
+use DB;
 
 class MoneyDetailController extends Controller
 {
     const DISP_MONTH = 5;
     public function index() {
-        $arrCategory = $this->getCategory();
-        $arrDispMonth = $this->createDispMonth();
-        $arrMoneyDetail = $this->createMoneyDetail($arrCategory, $arrDispMonth);
+        $arrCategory    = $this->getCategory();
+        $arrDispMonth   = $this->createDispMonth();
+        $arrPayData     = $this->getPayData($arrDispMonth);
+        $arrMoneyDetail = $this->createMoneyDetail($arrCategory, $arrDispMonth, $arrPayData);
 
         return view("money/detail", compact(
             'arrMoneyDetail',
@@ -23,7 +25,7 @@ class MoneyDetailController extends Controller
 
     private function getCategory() {
         $builder = Models\Category::select(['category', 'category_name']);
-        $builder = $builder->where('del_flg', '=', 0);
+        $builder->where('del_flg', '=', 0);
         
         return $builder->get()->toArray();
     }
@@ -42,7 +44,21 @@ class MoneyDetailController extends Controller
         return $arrDispMonth;
     }
 
-    private function createMoneyDetail($arrCategory, $arrDispMonth) {
+    private function getPayData($arrDispMonth) {
+        $startDate = date("Y-m-01 00:00:00", strtotime($arrDispMonth[0]. "/01 00:00:00"));
+        $endDate   = date("Y-m-01 00:00:00", strtotime($arrDispMonth[self::DISP_MONTH - 1]. "/01 00:00:00"));
+        // クレカ固定
+        $builder = Models\Management::select(DB::raw("IFNULL(sum(pay_bill), 0) as pay_bill"));
+        $builder->where('del_flg', '=', 0);
+        $builder->where('category_id', '=', 10);
+        $builder->whereBetween('pay_date', [$startDate, $endDate]);
+        $builder->groupBy('pay_date');
+        $builder->orderBy('pay_date');
+        
+        return $builder->get()->toArray();
+    }
+
+    private function createMoneyDetail($arrCategory, $arrDispMonth, $arrPayData) {
         foreach ($arrDispMonth as $key => $dispMonth) {
             $tmpTotal = 0;
             $tmpSalary = 0;
@@ -89,7 +105,11 @@ class MoneyDetailController extends Controller
                         $tmpTotal += $tmpData;
                         break;
                     case 'credit':
-                        $tmpData = 20000;
+                        if (array_key_exists($key, $arrPayData)) {
+                            $tmpData = $arrPayData[$key]['pay_bill'];
+                        } else {
+                            $tmpData = 0;
+                        }
                         $tmpTotal += $tmpData;
                         break;
                     case 'total':
--- resources/views/money/detail.blade.php
+++ resources/views/money/detail.blade.php
@@ -7,39 +7,35 @@
 @stop
 
 @section('content')
-<div class="card">
-    <div class="card-header">
-        金銭管理
-    </div>
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped table-hover datatable">
-                <tr>
-                    <th></th>
-                    @foreach ($arrDispMonth as $dispMonth)
-                    <th>{{ $dispMonth }}</th>
-                    @endforeach
-                </tr>
-                @foreach ($arrMoneyDetail as $moneyDetail)
-                <tr>
-                    <td>{{ $moneyDetail['category_name'] }}</td>
-                    @foreach ($moneyDetail['data'] as $detail)
-                    <td>{{ $detail }}</td>
-                    @endforeach
-                </tr>
-                @endforeach
-            </table>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card card-primary">
+            <div class="card-header">
+                金銭管理
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped table-hover datatable">
+                            <tr>
+                                <th></th>
+                                @foreach ($arrDispMonth as $dispMonth)
+                                <th>{{ $dispMonth }}</th>
+                                @endforeach
+                            </tr>
+                            @foreach ($arrMoneyDetail as $moneyDetail)
+                            <tr>
+                                <td>{{ $moneyDetail['category_name'] }}</td>
+                                @foreach ($moneyDetail['data'] as $detail)
+                                <td>{{ $detail }}</td>
+                                @endforeach
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
-
-    
 @stop
-
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-<!-- @section('js')
-    <script> console.log('Hi!'); </script>
-@stop -->
